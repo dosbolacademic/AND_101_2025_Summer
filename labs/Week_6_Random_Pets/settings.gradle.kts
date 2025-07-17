2251d548d26b0078130e7129a34a41ca3dfb78ba
Dosbol Aliev <DosbolAliev@gmIL.com>
2025-07-17 01:10:37 -0500

    Add Week 6 Random Pets lab

---
+++ labs/Week_6_Random_Pets/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Week_6_Lab_Random_Pets_part1"
+include(":app")
+ 
